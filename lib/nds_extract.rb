ee284b6e3a952f5ba3f5bf565f024d7bbb9ab96c
Emily Haynes <ehayne00@hotmail.com>
2019-11-05 19:24:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,15 +5,19 @@ require 'pry'
 def directors_totals(nds)
   binding.pry
   hash_names_totals = {}
-  row_index = 0 
-  while row_index < directors_database[row_index].count do
-    index = 0 
-    total = 0
-    while index < directors_database[row_index][:movies].count do
-      total += directors_database[row_index][:movies][index][:worldwide_gross]
-      index += 1
+  directors_database.each do |director|
+    director[:movies].each do |movie|
+    movie[:worldwide_gross]
     end
-    hash_names_totals[directors_database[row_index][:name]] = total
   end
+# # while row_index < directors_database[row_index].count do
+     
+#   #  total = 0
+#   # while index < directors_database[row_index][:movies].count do
+#     #  total += directors_database[row_index][:movies][index][:worldwide_gross]
+      
+#     #end
+#     #hash_names_totals[directors_database[row_index][:name]] = total
+#   end
   hash_names_totals
 end
